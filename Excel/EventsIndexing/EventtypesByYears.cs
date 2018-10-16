2583b1a70ec187b65b4f34a7a6292d0ca4773ae0
it4history <it4history@mail.ru>
2018-10-16 12:31:44 +0300

    format

--- Excel/EventsIndexing/EventtypesByYears.cs
+++ Excel/EventsIndexing/EventtypesByYears.cs
@@ -5,34 +5,38 @@ namespace Logy.Api.Mw.Excel
 {
     public static class EventtypesByYears
     {
-		/// <returns>graph; rowName, columnName, count</returns>
+        /// <returns>graph; rowName, columnName, count</returns>
         public static Dictionary<string, Dictionary<string, int>> Do(
-        	Dictionary<short, string> byYears,
-        	string filter = null) 
-		{
+            Dictionary<short, string> byYears,
+            string filter = null)
+        {
             var rows = new Dictionary<string, Dictionary<string, int>>();
-            foreach (var indices in byYears.Values) {
-	            var nodes = indices.Split(',').ToList()
-	            	.Distinct()
-	            	.Where(node => !string.IsNullOrEmpty(node) 
-	            	       && (filter == null || filter.Split(' ').Contains(node)));
-	            foreach(var node in nodes) {
-        			if (!rows.ContainsKey(node))
-        			{
-        				var list = new Dictionary<string, int>();
-        				rows.Add(node, list);
-        			}
-	            }
-	            
+            foreach (var indices in byYears.Values)
+            {
+                var nodes = indices.Split(',').ToList()
+                  .Distinct()
+                  .Where(node => !string.IsNullOrEmpty(node)
+                         && (filter == null || filter.Split(' ').Contains(node)));
+                foreach (var node in nodes)
+                {
+                    if (!rows.ContainsKey(node))
+                    {
+                        var list = new Dictionary<string, int>();
+                        rows.Add(node, list);
+                    }
+                }
+
                 // make complete graph
-	  			foreach (var node in nodes) {
-		  			foreach (var node2 in nodes) {
-                			var cols = rows[node];
-                			if (!cols.ContainsKey(node2))
-                				cols[node2] = 1;
-                			else
-                				cols[node2]++;
-                	}
+                foreach (var node in nodes)
+                {
+                    foreach (var node2 in nodes)
+                    {
+                        var cols = rows[node];
+                        if (!cols.ContainsKey(node2))
+                            cols[node2] = 1;
+                        else
+                            cols[node2]++;
+                    }
                 }
             }
 
